b1a920d3a31e25839fc6891ae1d9d60bd4a087d0
Mohammad Kamal <mohammadkamalshady@gmail.com>
2020-02-14 21:47:23 +0200

    First Commit

---
+++ Ta3allam Parmja/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Ta3allam_Parmja.Models;
+
+namespace Ta3allam_Parmja.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Introduction()
+        {
+            return View();
+        }
+
+        public IActionResult OpenGL()
+        {
+            return View("Getting Started/OpenGL");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Ta3allam Parmja/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ta3allam_Parmja.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
